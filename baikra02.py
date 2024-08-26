b79d1c4ec266d263c1de339fa7ca901c628a6101
HangLe05 <HangLe03112005@gmail.com>
2024-08-26 19:50:06 +0700

    Add files via upload

---
+++ baikra02.py
@@ -0,0 +1,16 @@
+k=int(input("nhap k"))
+n=0
+for i in range(1, 500):
+    sum=0
+    a=i
+    while i>0:
+        sum += i%10
+        i=i//10
+    if(sum==10):
+        n+=1
+    if(n==k):
+        print(a)
+        break;
+
+
+
